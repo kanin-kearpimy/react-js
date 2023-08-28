6a6bf5978cdcc9b27c2aa0070b1d63f79e493030
kanin-kearpimy <kanin.kearpimy@gmail.com>
2023-08-28 13:54:59 +0700

    adding exercise 4 - 5 to react 1

---
+++ react-1/exercise-3/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return <div id="app">
+    <RegistrationForm />
+    <DisplayMember />
+  </div>;
+}
+
+const RegistrationForm = () => {
+  return (
+    // Code here
+  );
+};
+
+const DisplayMember = () => {
+  return (
+    // Code here
+  );
+};
+
+export default App;
---
+++ react-1/exercise-4/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="app">
+      <h1>Enter Data</h1>
+      <RunningForm />
+      <TableDisplay />
+    </div>
+  );
+}
+
+export default App;
